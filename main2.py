fcf0212b41f1d3f7cac51c5d806e2f9b461b4f81
JohnExists <johnkolta9@gmail.com>
2024-01-19 18:15:53 -0500

    Fixed all issues

--- main2.py
+++ main2.py
@@ -1,26 +1,24 @@
 import socket
 import threading
 import tkinter as tk
-import time
-import random
 
 
 class Application:
 
     def __init__(self):
-        self.userID = random.randint(10000, 99999)
-        self.running = True
-
-        self.currentMessage = ""
+        # The messages sent to and from the other peroson
         self.messageStack = []
-        self.sendMessageStack = []
 
+        # Initializes the window and tkinter
         self.window = tk.Tk()
-        self.window.title("John Message")
+        self.window.title("John Message 2")
         self.window.configure(background='dark gray')
+        self.window.resizable(False, False)
 
+        # The variable for whatever the user typed into menu
         self.userInputVar = tk.StringVar()
 
+        # Setting up all the components of the window
         self.chatDisplay = None
         self.scroll_y = None
         self.userInputBox = None
@@ -28,65 +26,62 @@ class Application:
         self.server = None
         self.client = None
 
+        # Initializing all these components
         self.launchComponents()
 
+        # Setting up the connection to the server
         socket.setdefaulttimeout(0.01)
         thread = threading.Thread(target=(lambda: self.launchConnection()))
         thread.start()
 
-        # thread.join()
-
-        # self.launchComponents()
-        # self.launchConnection()
-        def test():
-            # print(len(self.messageStack))
+        # Loads any unread messages from the other person
+        def loadOthersMessages():
+            # Checks if theres any messages received from the other person
             self.launchChatUpdateChecker()
 
+            # If there is a message available
             while (len(self.messageStack) > 0):
+                # Remove it from the list and display it
                 newMessage = self.messageStack.pop()
                 self.chatDisplay.configure(state="normal")
                 self.chatDisplay.insert(tk.END, f"\n<Other> {newMessage}")
                 self.chatDisplay.configure(state="disabled")
 
                 self.chatDisplay.see("end")
+            # Call this function again after 1 ms
+            self.window.after(1, loadOthersMessages)
 
-            self.window.after(1, test)  # run this function again 2,000 ms from now
+        loadOthersMessages()
 
-            while len(self.sendMessageStack) > 0:
-                newMessage = self.sendMessageStack.pop()
-                try:
-                    self.client.send((newMessage).encode())
-                except:
-                    pass
-
-        test()
-
+        # Sets up the window close event and starts the main loops
+        self.window.protocol("WM_DELETE_WINDOW", lambda: self.on_closing())
         self.window.mainloop()
 
     def launchConnection(self):
+        # Attempts to connect to the server
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
             self.client.connect(('127.0.0.1', 4040))
         except:
+            # If no server is found :(
             print("<ERROR> Failed To Connect TO Server.")
             print("(hint: Please Make Sure Your Server Is Running)")
             return
 
-        # thread2 = threading.Thread(target=(lambda: self.launchChatUpdateChecker()))
-        # thread2.start()
-        # self.launchChatUpdateChecker()
-
     def launchComponents(self):
+        # Initializes the scrollbar
         self.scroll_y = tk.Scrollbar(self.window)
+        # Initializes the chat display
         self.chatDisplay = tk.Text(self.window, width=23, height=9, relief='groove', wrap='word',
                                    font=('arial', 25), yscrollcommand=self.scroll_y.set, background="dark gray")
-
         self.chatDisplay.insert(tk.END, f"Start of your conversation!")
         self.chatDisplay.configure(state="disabled")
 
         userInputBox = tk.Entry(self.window, textvariable=self.userInputVar, width=24, relief='groove',
                                 font=('arial', 20), background="dark gray")
 
+        # Functions are called when the user press the "Enter" key 
+        # or clicks the "Send" button
         def callback():
             self.onSend()
             userInputBox.delete(0, tk.END)
@@ -96,8 +91,10 @@ class Application:
 
         self.window.bind('<Return>', func)
 
+        # Initializes the "Send" button
         sendButton = tk.Button(self.window, text="Send", command=callback)
 
+        # Places all the components in the right place
         self.scroll_y.pack(side=tk.RIGHT, fill=tk.Y)
         self.chatDisplay.pack(fill=tk.X)
         userInputBox.pack(side=tk.LEFT)
@@ -106,32 +103,42 @@ class Application:
         self.scroll_y.config(command=self.chatDisplay.yview)
 
     def launchChatUpdateChecker(self):
-        # self.client.send(("RECEIVED").encode())
-        # while True:
+        # Checks if theres any messages received from the other person
         if (self.client is not None):
             try:
+                # Sends something to receive something back
+                self.client.send(("RECEIVED").encode())
+                # Reads the message from server
                 from_server = self.client.recv(4096)
                 message = from_server.decode()
-                print(message)
-                # thread = threading.Thread(target=(lambda: receive()))
-                # thread.start()
+                # If there is a message append it to the message Stack
                 if (message != ""):
                     self.messageStack.append(message)
             except Exception as exception:
                 pass
-        # threading.Timer(1, (lambda: self.launchChatUpdateChecker())).start()
 
     def onSend(self):
+        # Gets the input from the user
         userInput = self.userInputVar.get()
         if (userInput == '' or userInput == ' '): return
 
+        # Adds the users message to the chat
         self.chatDisplay.configure(state="normal")
         self.chatDisplay.insert(tk.END, "\n<You> " + userInput)
         self.chatDisplay.configure(state="disabled")
 
-        self.sendMessageStack.append(userInput)
+        # Sends the input to the server
+        try:
+            self.client.send((userInput).encode())
+        except:
+            pass
+
 
         self.chatDisplay.see("end")
 
+    def on_closing(self):
+        # Closes the application
+        self.window.destroy()
+        self.client.close()
 
 application = Application()
--- server.py
+++ server.py
@@ -1,41 +1,17 @@
-# import socket
-# import time
-# serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# serv.bind(('127.0.0.1', 4040))
-# serv.listen(5)
-#
-# while True:
-#     conn, addr = serv.accept()
-#     while True:
-#         data = conn.recv(4096)
-#         if not data: break
-#
-#         value = data.decode('utf8')
-#         if(value != "RECEIVED"): print(f'From client: {value}')
-#
-#         conn.send("Hello!".encode())
-#         time.sleep(4)
-#     conn.close()
-#
-# print('client disconnected and shutdown')
-# !/usr/bin/python           # This is server.py file
-
 import socket  # Import socket module
 import threading
-import time
-
 
 unreadMessageParty1 = []
 unreadMessageParty2 = []
 
-def on_new_client(conn, addr, number):
+def on_new_client(conn, number):
     while True:
         data = conn.recv(4096)
         if not data: break
 
         value = data.decode('utf8')
         if (value != "RECEIVED"):
-            print(f'From client: {value}')
+            print(f'From client {number}: {value}')
             if(number == 0):
                 unreadMessageParty2.append(value)
             if(number == 1):
@@ -51,6 +27,9 @@ def on_new_client(conn, addr, number):
                 conn.send(message.encode())
         # time.sleep(0.01)
     conn.close()
+    unreadMessageParty1.clear()
+    unreadMessageParty2.clear()
+    activeThreads.clear()
 
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create a socket object
@@ -65,7 +44,7 @@ activeThreads = []
 
 while True:
     conn, addr = server.accept()  # Establish connection with client.
-    thread = threading.Thread(target=(lambda: on_new_client(conn, addr, len(activeThreads))))
+    thread = threading.Thread(target=(lambda: on_new_client(conn, len(activeThreads))))
     thread.start()
     activeThreads.append(thread)
 
