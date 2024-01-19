7c3ff53128dae361f32027ceda712f9221eb0583
JohnExists <johnkolta9@gmail.com>
2024-01-19 18:18:14 -0500

    commented Server.py

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import socket  # Import socket module
 import threading
 
+# The unread messages for each party
 unreadMessageParty1 = []
 unreadMessageParty2 = []
 
@@ -10,13 +11,14 @@ def on_new_client(conn, number):
         if not data: break
 
         value = data.decode('utf8')
+        # If the server received a proper message from the client
         if (value != "RECEIVED"):
             print(f'From client {number}: {value}')
-            if(number == 0):
-                unreadMessageParty2.append(value)
-            if(number == 1):
-                unreadMessageParty1.append(value)
+            # Send that message to the other party
+            if(number == 0): unreadMessageParty2.append(value)
+            if(number == 1): unreadMessageParty1.append(value)
 
+        # Send any unread messages to the client
         if(number == 0):
             if(len(unreadMessageParty1) > 0):
                 message = unreadMessageParty1.pop()
@@ -25,13 +27,12 @@ def on_new_client(conn, number):
             if(len(unreadMessageParty2) > 0):
                 message = unreadMessageParty2.pop()
                 conn.send(message.encode())
-        # time.sleep(0.01)
     conn.close()
     unreadMessageParty1.clear()
     unreadMessageParty2.clear()
     activeThreads.clear()
 
-
+# Initialize the server
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create a socket object
 server.bind(('127.0.0.1', 4040))
 
@@ -42,6 +43,7 @@ server.listen(5)  # Now wait for client connection.
 
 activeThreads = []
 
+# Checks for all the clients that connected
 while True:
     conn, addr = server.accept()  # Establish connection with client.
     thread = threading.Thread(target=(lambda: on_new_client(conn, len(activeThreads))))
